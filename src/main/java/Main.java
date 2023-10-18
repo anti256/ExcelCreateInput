27b5490756e4ec6b5f95e9c06d571db40f4e9592
anti256 <anti256@mail.ru>
2023-10-18 15:32:33 +0300

    code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import java.io.File;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //создание объекта для связи с Excel
         XSSFWorkbook workbook = new XSSFWorkbook();
 
@@ -15,21 +21,36 @@ public class Main {
         //объект строка
         XSSFRow row;
 
-        //счетчик строк
-        int rowid = 0;
-
-        for (int i = 0; i < 10; i++) {
-            row = spreadsheet.createRow(rowid++);
-            //int cellid = 0;//счетчик колонок
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < 10; i++) {//перебор строк
+            row = spreadsheet.createRow(i);
+            for (int j = 0; j < 10; j++) {//перебор колонок
                 Cell cell = row.createCell(j);
-                cell.setCellValue(i + "/" + j);
+                cell.setCellValue((i+1) + "/" + (j+1));
             }
         }
 
-        FileOutputStream out = new FileOutputStream(
-                new File("C:/savedexcel/myExcelFile.xlsx"));
+        //запись в файл
+        File file = new File("F:\\myExcelFile.xlsx");
+        FileOutputStream out = new FileOutputStream(file);
 
+        workbook.write(out);
+        out.close();
+
+
+        row = spreadsheet.getRow(5);
+        Cell cell = row.getCell(8);
+
+        cell.getRow().setHeightInPoints(cell.getSheet().getDefaultRowHeightInPoints() * 2);
+
+        CellStyle cellStyle = cell.getSheet().getWorkbook().createCellStyle();
+        cellStyle.setWrapText(true);
+        cell.setCellStyle(cellStyle);
+
+        cell.setCellValue("изменение ячейки\nвторая строка");
+
+        spreadsheet.autoSizeColumn(8);
+
+        out = new FileOutputStream(file);
         workbook.write(out);
         out.close();
     }
